ec6a688ab0861c721ad363bf2232a779d06c19e9
Elvis Addae <elvis.m.addae@hotmail.com>
2020-05-06 14:24:09 +0000

    Before Branch

---
+++ CleanArch.Mvc/Controllers/CourseController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CleanArch.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArch.Mvc.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly ICourseService _courseService;
+
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        public IActionResult Index()
+        {
+            var viewModel = _courseService.GetCourses();
+
+            return View(viewModel);
+        }
+    }
+}
