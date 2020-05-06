c93cd5ee3fe6cb9128215033459e273598ed74c1
Elvis Addae <elvis.m.addae@hotmail.com>
2020-05-06 09:48:38 +0000

    Commit Implementation of service and repository

---
+++ CleanArch.Application/Services/CourseService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using CleanArch.Domain.Interfaces;
+
+namespace CleanArch.Application.Services
+{
+    public class CourseService : ICourseService
+    {
+        private readonly ICourseRepository _courseRepository;
+        public CourseService(ICourseRepository courseRepository)
+        {
+            _courseRepository = courseRepository;
+        }
+
+        public CoursesViewModel GetCourses()
+        {
+            return new CoursesViewModel {Courses = _courseRepository.GetCourses()};
+        }
+    }
+}
---
+++ CleanArch.Infra.Data/Repository/CourseRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CleanArch.Domain.Interfaces;
+using CleanArch.Domain.Models;
+
+namespace CleanArch.Infra.Data.Repository
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private readonly UniversityDbContext _dbContext;
+
+        public CourseRepository(UniversityDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IEnumerable<Course> GetCourses()
+        {
+            return _dbContext.Courses;
+        }
+    }
+}
